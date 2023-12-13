0dcc971fb979ae45f52b083cff8fc87feac19fb5
Denisa Kita <denisakita7@gmail.com>
2023-12-13 15:33:54 +0100

    array setState value - 013

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+
+const taskList = [
+    {id: 1000, name: "Test1", completed: true},
+    {id: 2000, name: "Test2", completed: false},
+    {id: 3000, name: "Test3", completed: true},
+]
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [tasks, setTasks] = useState(taskList)
+
+
+    function handleDelete(id) {
+        setTasks(tasks.filter(task =>task.id !== id))
+    }
+
+    return (
+        <div className="App">
+            <h1>Tasks List</h1>
+            <ul>
+                {tasks.map((task) => (
+                    <li key={task.id}>
+                        <span>{task.id} - {task.name}</span>
+                        <button onClick={()=>handleDelete(task.id)} className="delete">Delete</button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
