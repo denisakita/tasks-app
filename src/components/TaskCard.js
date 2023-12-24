bf92b81253db73cc8dadce6d6656a00b382d6fe0
Denisa Kita <denisakita7@gmail.com>
2023-12-24 23:40:04 +0100

    component level styling - 021

--- src/components/TaskCard.js
+++ src/components/TaskCard.js
@@ -1,7 +1,9 @@
-export const TaskCard = ({task, handleDelete}) => {
+import "./TaskCard.css";
+
+export const TaskCard = ({ task, handleDelete }) => {
     return (
         <div className="taskcard">
-            <li className={task.completed ? "completed" : "incomplete"}>
+            <li className={ task.completed ? "completed" : "incomplete" }>
                 <span>{task.id} - {task.name}</span>
                 <button onClick={() => handleDelete(task.id)} className='delete'>Delete</button>
             </li>
