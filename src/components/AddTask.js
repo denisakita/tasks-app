61fa6555f91866f09499579a7813a7066448b6df
Denisa Kita <denisakita7@gmail.com>
2023-12-28 14:47:56 +0100

    onSubmit event - 025

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -5,6 +5,9 @@ export const AddTask = () => {
 
     const [taskValue, setTaskValue] = useState('');
 
+    // 1. Add state for boolean value completed
+    const [progress, setProgress] = useState(false);
+
     const handleChange = (event) => {
         setTaskValue(event.target.value);
 
@@ -12,17 +15,40 @@ export const AddTask = () => {
 
     const handleReset = () => {
         setTaskValue("");
+        // 2. Add state for boolean value completed at reset
+        setProgress(false);
+    }
+
+    // 4. Create onSubmit method for form
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const task = {
+            id: Math.floor(Math.random() * 1000),
+            name: taskValue,
+            completed: Boolean(progress)
+        }
+        console.log(task);
+        handleReset();
     }
 
     return (
         <section className="addtask">
-            <form>
+            {/*3. Create onSubmit method for form*/}
+            <form onSubmit={handleSubmit}>
                 <input onChange={handleChange} type="text" name="task" id="task"
                        placeholder="Task Name" value={taskValue}/>
-                <button type="submit">Add Task</button>
+
+                {/*5. Add select with 2 values*/}
+                <select onChange={(event) => setProgress(event.target.value)} value={progress}>
+                    <option value="false">Pending</option>
+                    <option value="true">Completed</option>
+                </select>
+
                 <span onClick={handleReset} className="reset">Reset</span>
+                <button type="submit">Add Task</button>
+
             </form>
-            <p>{taskValue}</p>
 
         </section>
     )
