ff374f4eeb686ca82f1c0c9646b456930c7b260c
Denisa Kita <denisakita7@gmail.com>
2023-12-28 14:06:48 +0100

    controlled inputs - 025

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -9,14 +9,21 @@ export const AddTask = () => {
         setTaskValue(event.target.value);
 
     }
+
+    const handleReset = () => {
+        setTaskValue("");
+    }
+
     return (
         <section className="addtask">
             <form>
-                <input onChange={handleChange} type="text" name="task" id="task" placeholder="Task Name"/>
+                <input onChange={handleChange} type="text" name="task" id="task"
+                       placeholder="Task Name" value={taskValue}/>
                 <button type="submit">Add Task</button>
+                <span onClick={handleReset} className="reset">Reset</span>
             </form>
             <p>{taskValue}</p>
-            <p>{taskValue.length}</p>
+
         </section>
     )
 }
