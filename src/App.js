fa183d0aef9549269ea92982a5026973e2361281
Denisa Kita <denisakita7@gmail.com>
2023-12-29 16:41:55 +0100

    list output - 035

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
+import { ProductList } from './components/ProductList';
 import './App.css';
-import TaskList from "./components/TaskList";
-import {Header} from "./components/Header";
-import {Footer} from "./components/Footer";
-import {AddTask} from "./components/AddTask";
-import {useState} from "react";
-
-const taskList = [
-    {id: 1000, name: "Test1", completed: true},
-    {id: 2000, name: "Test2", completed: false},
-    {id: 3000, name: "Test3", completed: true},
-]
 
 function App() {
-    const [tasks, setTasks] = useState([]);
-
     return (
         <div className="App">
-            <Header/>
-            <main>
-                <AddTask tasks={tasks} setTasks={setTasks}/>
-                <TaskList tasks={tasks} setTasks={setTasks}/>
-            </main>
-            <Footer/>
+            <h1>Product List</h1>
+            <ProductList />
         </div>
     );
 }
