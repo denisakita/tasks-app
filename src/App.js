49dfcd05ff479ce6259976f8349d9d6b1d227047
Denisa Kita <denisakita7@gmail.com>
2024-01-04 16:06:30 +0100

    change structure of app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { ProductList } from './components/ProductList';
 import './App.css';
+import {Header} from "./components/Header";
+import {Footer} from "./components/Footer";
 
 function App() {
     return (
         <div className="App">
+            <Header/>
             <h1>Product List</h1>
             <ProductList />
+            <Footer/>
         </div>
     );
 }
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,44 +1,8 @@
 import {useState} from "react"
-import {useFetch} from "../hooks/useFetch";
 
 export const ProductList = () => {
-    // const [products, setProducts] = useState([]);
+
     const [url, setUrl] = useState("http://localhost:3000/products");
-    const {data: products, loading, error} = useFetch(url);
-
-    // 3. fetch as a function outside useEffect
-
-    // const fetchProducts = useCallback(
-    //     async () => {
-    //         const response = await fetch(url);
-    //         const data = await response.json();
-    //         setProducts(data);
-    //     }, [url]
-    // );
-
-    // useEffect(() => {
-    //     fetchProducts();
-    // }, [fetchProducts]);
-
-    // 2. fetch as a function inside useEffect
-
-    // useEffect(() => {
-    //         const fetchProducts = async () => {
-    //             const response = await fetch(url);
-    //             const data = await response.json();
-    //             setProducts(data);
-    //         }
-    //         fetchProducts();
-    //     }, [url]
-    // );
-
-    // 1. fetch inside useEffect
-
-    // useEffect(() => {
-    //     fetch(url)
-    //         .then(response => response.json())
-    //         .then(data => setProducts(data));
-    // }, [url]);
 
 
     return (
@@ -48,20 +12,6 @@ export const ProductList = () => {
                 <button onClick={() => setUrl("http://localhost:3000/products?in_stock=true")}>In Stock Only</button>
             </div>
 
-            {loading && <p>Loading products ...</p>}
-            {error && <p>{error}</p>}
-
-            {products && products.map((product) => (
-                <div className="card" key={product.id}>
-                    <p className="id">{product.id}</p>
-                    <p className="name">{product.name}</p>
-                    <p className="info">
-                        <span>${product.price}</span>
-                        <span
-                            className={product.in_stock ? "instock" : "unavailable"}>{product.in_stock ? "In Stock" : "Unavailable"}</span>
-                    </p>
-                </div>
-            ))}
         </section>
     )
 }
